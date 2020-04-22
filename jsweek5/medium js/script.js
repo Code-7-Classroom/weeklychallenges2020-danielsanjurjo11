4f86887d0101c1083f5668bb493b93d832221464
daniel sanjurjo <61425075+danielsanjurjo11@users.noreply.github.com>
2020-04-21 23:16:05 -0400

    Add files via upload

---
+++ jsweek5/medium js/script.js
@@ -0,0 +1,5 @@
+let values = [1, 2, 3, 4, 5];
+let sum = values.reduce((a, b) => a += b);
+let avg = sum / values.length;
+
+console.log(avg);
---
+++ jsweek5/veryeasy:easy js/script.js
@@ -0,0 +1,8 @@
+//VERY EASY: What is the difference between the following JavaScript keywords: var, let and const.  Please provide a written explanation. You are able to include a code reference along with your written explanation for each keyword.
+//var variables can be updated and re-declared within its scope
+//let variables can be updated but not re-declared
+//const variables can neither be updated nor re-declared.
+
+
+const newArry = [1, 2, 3, 4]
+console.log(Math.max(...newArry));
